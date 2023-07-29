d28384044d872178c2c981fd365d001eeecc2fd6
Cameron Lee <camdlee18@gmail.com>
2023-07-28 22:18:02 -0400

    added navbar template and bootstrap css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './views/Home';
+import NavbarScroll from './components/Nav';
 
 
 export default function App() {
 
   return(
     <BrowserRouter>
+      <NavbarScroll/>
       <Routes>
         <Route path='/' element={<Home/>}/>
 
