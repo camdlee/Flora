09e5192f8303a0b22bd163ebd057c8fc378b6994
Cameron Lee <camdlee18@gmail.com>
2023-07-28 21:52:03 -0400

    created useEffect hook to pull data from plant API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React from "react";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
+import Home from './views/Home';
 
 
 export default function App() {
 
   return(
     <BrowserRouter>
-      <Route/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+      </Routes>
     </BrowserRouter>
   )
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import { useState, useEffect } from 'react';
 
 
 export default function Home() {
+    console.log('home page')
+    // --------------- Initializing variables ----------
+    const apiUrl = `https://perenual.com/api/species-list?page=1&key=sk-NXut64b9f2491050e1633`
+    const [plants, setPlants] = useState([])
+
+
+    // testing out the Perenual Plant API
+    // key = sk-NXut64b9f2491050e1633
+    useEffect(() => {
+        // console.log('useEffect hook to fetch api data')
+        // fetching data from api
+        fetch(apiUrl)
+        // waiting for response
+        .then(response => response.json())
+        // set state of plants with data pulled from api
+        .then(data=>{
+            console.log(data)
+            setPlants(data.results)
+        })
+    })
 
     return(
-
+        <>
+            <div className='home-container'>
+                <h1>This is the home page</h1>
+            </div>
+        </>
     )
 }
