60de4a8aa9d43bc8ddba3fdb7f6676ccba8ce98e
Cameron Lee <camdlee18@gmail.com>
2023-07-30 14:26:43 -0400

    created plant card, able to save plant data and map on card

---
+++ src/components/PlantCard.js
@@ -0,0 +1,45 @@
+//------------------- IMPORTS ----------------
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+export default function PlantCard({key, name, other_name, cycle, image, scientific_name, sunlight, watering}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title={name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography gutterBottom variant="h6" component="div">
+          {scientific_name}
+        </Typography>
+        <Typography gutterBottom variant="h6" component="div">
+          {other_name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {cycle}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {sunlight}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {watering}
+        </Typography>
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
+  );
+}
--- src/views/PlantData.js
+++ src/views/PlantData.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-
+import PlantCard from '../components/PlantCard';
 
 export default function PlantData() {
 
     // --------------- Initializing variables ----------
     const apiUrl = `https://perenual.com/api/species-list?page=1&key=sk-NXut64b9f2491050e1633`
-    const [plants, setPlants] = useState([])
-
+    const [plantData, setPlantData] = useState([])
+    const [plant, setPlant] = useState('')
 
     // testing out the Perenual Plant API
     // key = sk-NXut64b9f2491050e1633
@@ -19,11 +19,12 @@ export default function PlantData() {
         .then(response => response.json())
         // set state of plants with data pulled from api
         .then(data=>{
-            console.log(data['data'])
-            // setPlants(data['data'])
+            console.log(data.data)
+            setPlantData(data.data)
         })
-    },)
+    },[plant])
 
+    // console.log(plantData[0].default_image.original_url)
 
     return(
         <>
@@ -32,11 +33,21 @@ export default function PlantData() {
                 <h1>This is the plant data page</h1>
             </div>
             <div>
-                {/* {plants.map((data)=>{
+                {plantData.map(plant => {
                     return(
-                        <p>{data.data}</p>
+                        <PlantCard
+                        key={plant.id}
+                        name={plant.common_name}
+                        other_name={plant.other_name}
+                        scientific_name={plant.scientific_name}
+                        cycle={plant.cycle}
+                        // image={plant.default_image.thumbnail}
+                        sunlight={plant.sunlgiht}
+                        watering={plant.watering}
+                        />
                     )
-                })} */}
+                })
+                }
             </div>
         </body>
         </>
